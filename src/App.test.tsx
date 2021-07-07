1d1c65876ffd0e674bb880c72dec3892cad8db7a
tommytommychopper <t.itsuki928@gmail.com>
2021-07-07 22:54:53 +0900

    [WIP] test component

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('Render App Component', () => {
+    test('should render header', () => {
+        setTimeout(() => {
+            // const { getByTestId } = render(<App />);
+            // const headerEl = getByTestId('header');
+            // expect(headerEl.textContent).toBe('Time Zone Covnerter');
+        }, 500);
+    });
+});
---
+++ src/App/TimeZoneRow/index.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TimeZoneRow, { IProps } from './index';
+
+// let getByTestId;
+// beforeEach(() => {
+//     const component = render(< TimeZoneRow selected={''} value={''} setSelected={} onChange={} />;
+//     getByTestId = component.getByTestId;
+// });
+
+describe('check TimeZoneRow component works correctly', () => {
+    it('should render header', () => {});
+});
