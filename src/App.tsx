3b0e5e17adbeaf7a3d3d64f71fdd2858d44701f5
tommytommychopper <t.itsuki928@gmail.com>
2021-07-07 22:56:17 +0900

    [add] functionarity to TimeZoneRow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import GlobalStyle from './style/Global';
 import TimeZoneRow from './App/TimeZoneRow/index';
@@ -7,12 +7,119 @@ const Header = styled.h1`
     font-size: 35px;
     font-style: italic;
 `;
-const App: React.FC = () => (
-    <>
-        <GlobalStyle />
-        <Header> Time Zone Covnerter</Header>
-        <TimeZoneRow />
-        <TimeZoneRow />
-    </>
-);
+
+const ApiKey = process.env.REACT_APP_DATE_API;
+
+function App(): JSX.Element {
+    const [fromSelected, setFromSelected] = useState('JP');
+    const [toSelected, setToSelected] = useState('US');
+    const [fromDate, setFromDate] = useState('');
+    const [toDate, setToDate] = useState('');
+    useEffect(() => {
+        const tzFrom = fromSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        const tzTo = toSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        fetch(`https://api.ipgeolocation.io/timezone?apiKey=${ApiKey}&location=${tzFrom}`)
+            .then((res) => res.json())
+            .then((res) => {
+                fetch(
+                    `https://api.ipgeolocation.io/timezone/convert?apiKey=${ApiKey}&tz_from=${tzFrom}&tz_to=${tzTo}&time=${res.date}%20${res.time_24}`,
+                )
+                    .then((data) => data.json())
+                    .then((data) => {
+                        let originalDate: string = data.original_time;
+                        originalDate = originalDate.replace(' ', 'T').slice(0, -3);
+                        let convertedDate: string = data.converted_time;
+                        convertedDate = convertedDate.replace(' ', 'T').slice(0, -3);
+                        setFromDate(originalDate);
+                        setToDate(convertedDate);
+                    });
+            });
+    }, []);
+
+    useEffect(() => {
+        async function updateDate(): Promise<void> {
+            const tzFrom = fromSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+            const tzTo = toSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+            const dateArr = fromSelected.split('T');
+            const date = dateArr[0];
+            const time = dateArr[1];
+            await fetch(
+                `https://api.ipgeolocation.io/timezone/convert?apiKey=${ApiKey}&tz_from=${tzFrom}&tz_to=${tzTo}&time=${date}%20${time}`,
+            )
+                .then((res) => res.json())
+                .then((res) => {
+                    let originalDate: string = res.original_time;
+                    originalDate = originalDate.replace(' ', 'T');
+                    let convertedDate: string = res.converted_time;
+                    convertedDate = convertedDate.replace(' ', 'T');
+                    setFromDate(originalDate);
+                    setToDate(convertedDate);
+                });
+        }
+        updateDate();
+    }, [fromSelected, toSelected]);
+
+    async function handleFromDateChange(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): Promise<void> {
+        const tzFrom = fromSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        const tzTo = toSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        const dateArr = e.target.value.split('T');
+        const date = dateArr[0];
+        const time = dateArr[1];
+        await fetch(
+            `https://api.ipgeolocation.io/timezone/convert?apiKey=${ApiKey}&tz_from=${tzFrom}&tz_to=${tzTo}&time=${date}%20${time}`,
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                let originalDate: string = res.original_time;
+                originalDate = originalDate.replace(' ', 'T');
+                let convertedDate: string = res.converted_time;
+                convertedDate = convertedDate.replace(' ', 'T');
+                setFromDate(originalDate);
+                setToDate(convertedDate);
+            });
+    }
+
+    async function handleToDateChange(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): Promise<void> {
+        const tzFrom = fromSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        const tzTo = toSelected === 'JP' ? 'Japan' : 'America/Los_Angeles';
+        const dateArr = e.target.value.split('T');
+        const date = dateArr[0];
+        const time = dateArr[1];
+        await fetch(
+            `https://api.ipgeolocation.io/timezone/convert?apiKey=${ApiKey}&tz_from=${tzTo}&tz_to=${tzFrom}&time=${date}%20${time}`,
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                let originalDate: string = res.original_time;
+                originalDate = originalDate.replace(' ', 'T');
+                let convertedDate: string = res.converted_time;
+                convertedDate = convertedDate.replace(' ', 'T');
+                setFromDate(convertedDate);
+                setToDate(originalDate);
+            });
+    }
+
+    if (fromDate && toDate) {
+        return (
+            <>
+                <GlobalStyle />
+                <Header data-testid="header">Time Zone Covnerter</Header>
+                <TimeZoneRow
+                    selected={fromSelected}
+                    setSelected={setFromSelected}
+                    value={fromDate}
+                    onChange={handleFromDateChange}
+                />
+                <TimeZoneRow
+                    selected={toSelected}
+                    setSelected={setToSelected}
+                    value={toDate}
+                    onChange={handleToDateChange}
+                />
+            </>
+        );
+    }
+    return <> </>;
+}
+
 export default App;
--- src/App/TimeZoneRow/index.tsx
+++ src/App/TimeZoneRow/index.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { TextField } from '@material-ui/core';
 import ReactFlagsSelect from 'react-flags-select';
 import Wrapper from './Wrapper';
 
-const TimeZoneRow: React.FC = () => {
-    const [selected, setSelected] = useState('');
-    return (
-        <Wrapper paddingTop={30}>
-            <TextField id="datetime-local" type="datetime-local" defaultValue="2021-07-01T16:30" />
-            <ReactFlagsSelect
-                selected={selected}
-                onSelect={(code) => setSelected(code)}
-                countries={['JP', 'US']}
-                // selectedSize={18}
-            />
-        </Wrapper>
-    );
-};
+export interface IProps {
+    selected: string;
+    setSelected: React.Dispatch<React.SetStateAction<string>>;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => Promise<void>;
+}
 
+const TimeZoneRow: React.FC<IProps> = ({ selected, setSelected, value, onChange }) => (
+    <Wrapper paddingTop={30}>
+        <TextField id="datetime-local" type="datetime-local" value={value} onChange={onChange} />
+        <ReactFlagsSelect
+            selected={selected}
+            onSelect={(code) => setSelected(code)}
+            countries={['JP', 'US']}
+            customLabels={{ JP: 'JP', US: 'US' }}
+        />
+    </Wrapper>
+);
 export default TimeZoneRow;
