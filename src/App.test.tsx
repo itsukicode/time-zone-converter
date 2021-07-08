0d42d1f05f260b7f0931569e7d96ae9d7f2d6360
tommytommychopper <t.itsuki928@gmail.com>
2021-07-08 10:38:00 +0900

    [delete] comment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,9 +5,9 @@ import App from './App';
 describe('Render App Component', () => {
     test('should render header', () => {
         setTimeout(() => {
-            // const { getByTestId } = render(<App />);
-            // const headerEl = getByTestId('header');
-            // expect(headerEl.textContent).toBe('Time Zone Covnerter');
+            const { getByTestId } = render(<App />);
+            const headerEl = getByTestId('header');
+            expect(headerEl.textContent).toBe('Time Zone Covnerter');
         }, 500);
     });
 });
